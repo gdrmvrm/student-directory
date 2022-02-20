d4a5a54d3a4c73d8787643cb87edec300399c717
GDRMVRM <gdrmvrm@gmail.com>
2022-02-20 11:33:53 +0000

    Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,67 +1,68 @@
+@students = []
 def interactive_menu
-  students = []
   loop do
-  # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  # 2. read the input and save it into a variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-      when "1"
-        students = student + input_students 
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit #this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-      end
-  # 4. repeat from step 1
+    print_menu
+    process(gets.chomp)
   end 
 end
 
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end 
+
+def process(selection)
+  case selection
+  when "1"
+     input_students 
+  when "2"
+      show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
 
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # gets another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end 
 
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 # nothing happens until we call the methods
 print interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
